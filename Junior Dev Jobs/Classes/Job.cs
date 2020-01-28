6def22aba5ddbaafba58eecf2e33cab0573f4a7c
Kris Alcordo <kjaonline@gmail.com>
2020-01-28 15:09:14 -0500

    Add project files.

---
+++ Junior Dev Jobs/Classes/Job.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Junior_Dev_Jobs.Classes
+{
+    public class Job
+    {
+        private int Id { get; set; }
+        private String Job_name { get; set; }
+        private String Job_description { get; set; }
+        private double Salary { get; set; }
+
+        public void Job_information(string name, string description, double salary)
+        {
+            name = this.Job_name;
+            description = this.Job_description;
+            salary = this.Salary;
+        }
+    }
+}   
---
+++ Junior Dev Jobs/Classes/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Junior_Dev_Jobs.Classes
+{
+    public class User
+    {
+        private int Id { get; set; }
+        private String First_name { get; set; }
+        private String Last_name { get; set; }
+        private String Email { get; set; }
+        private Application Application { get; set; }
+    }
+}
